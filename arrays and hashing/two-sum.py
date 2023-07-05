5cb827ac1a4e20996cc9d3d240f29d96addd6f44
Isaac Mohammed Nur <135744887+imohamnur@users.noreply.github.com>
2023-07-04 23:52:01 -0500

    Add files via upload

---
+++ arrays and hashing/two-sum.py
@@ -0,0 +1,7 @@
+def twoSum(self, nums: List[int], target: int) -> List[int]:
+        nmap = {}
+
+        for i in range(len(nums)):
+            if target - nums[i] in nmap:
+                return [i, nmap[target - nums[i]]]
+            nmap[nums[i]] = i
