76259f691c30bf11680cfe9e019fda1f95d264ae
Isaac Mohammed Nur <135744887+imohamnur@users.noreply.github.com>
2023-07-11 00:03:22 -0500

    Add files via upload

---
+++ arrays and hashing/217. Contains Duplicate.py
@@ -0,0 +1,7 @@
+def containsDuplicate(self, nums: List[int]) -> bool:
+        numset = set()
+        for n in nums:
+            if n in numset:
+                return True
+            numset.add(n)
+        return False
